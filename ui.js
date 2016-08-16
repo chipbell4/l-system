27aed873f42aea206de18a453238368180766695
Chip Bell <cbell@pbs.org>
2016-08-16 12:43:42 -0400

    Wiring up chaos fields

--- ui.js
+++ ui.js
@@ -12,6 +12,11 @@ UI = {
   redraw: function() {
     var ruleset = LSystem.parseFromDom();
 
+    // update the turtle with parameters
+    Turtle.ANGLE = Number(document.getElementById('angle').value);
+    Turtle.ANGLE_CHAOS = Number(document.getElementById('angle-chaos').value);
+    Turtle.DISTANCE_CHAOS = Number(document.getElementById('distance-chaos').value);
+
     var production = document.getElementById('axiom').value;
     for(var i = 0; i < 10; i++) {
       production = LSystem.stepSystem(production, ruleset);
