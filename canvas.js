557a9d8e72f5cd2b63000555f9a1b99e95e7bd93
Chip Bell <cbell@pbs.org>
2016-08-16 10:01:42 -0400

    Trying out some centering logic

--- canvas.js
+++ canvas.js
@@ -5,10 +5,13 @@ var Canvas = {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+
     ctx.beginPath();
-    ctx.moveTo(path[0].x, path[0].x);
+    ctx.moveTo(path[0].x + centerX, path[0].y + centerY);
     for(var i = 1; i < path.length; i++) {
-      ctx.lineTo(path[i].x, path[i].y);
+      ctx.lineTo(path[i].x + centerX, path[i].y + centerY);
     }
     ctx.stroke();
   }
