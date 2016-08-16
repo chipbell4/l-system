3d0df91c61959078fbd3e55ee990b93b666e58ee
Chip Bell <cbell@pbs.org>
2016-08-16 09:10:27 -0400

    Adding removal of rules

--- ui.js
+++ ui.js
@@ -2,6 +2,11 @@ UI = {
   addRule: function() {
     var template = document.querySelector('#rule-template > .rule').cloneNode(true);
     document.getElementById('rules').appendChild(template);
+    template.querySelector('.remove-rule').addEventListener('click', UI.removeRule.bind(UI));
+  },
+
+  removeRule: function(evt) {
+    evt.target.parentElement.remove();
   },
 
   init: function() {
