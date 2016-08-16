cf05b8fa9fa861ffc03a3a7554440826ebd2d88e
Chip Bell <cbell@pbs.org>
2016-08-16 10:12:03 -0400

    Better rescaling

--- canvas.js
+++ canvas.js
@@ -8,10 +8,32 @@ var Canvas = {
     var centerX = canvas.width / 2;
     var centerY = canvas.height / 2;
 
+    // get bounding points
+    var xValues = path.map(point => point.x);
+    var yValues = path.map(point => point.y);
+    var minX = xValues.reduce((best, current) => Math.min(best, current), xValues[0]);
+    var maxX = xValues.reduce((best, current) => Math.max(best, current), xValues[0]);
+    var minY = yValues.reduce((best, current) => Math.min(best, current), yValues[0]);
+    var maxY = yValues.reduce((best, current) => Math.max(best, current), yValues[0]);
+
+    // pick scaling factors (preserving aspect ratio)
+    var scaleX = canvas.width / (maxX - minX);
+    var scaleY = canvas.height / (maxY - minY);
+    scaleY = Math.min(scaleY, scaleX);
+    scaleX = Math.min(scaleY, scaleX);
+
+    // now, rescale
+    path = path.map(function(point) {
+      return {
+        x: (point.x - minX) * scaleX,
+        y: (point.y - minY) * scaleY,
+      };
+    });
+
     ctx.beginPath();
     ctx.moveTo(path[0].x + centerX, path[0].y + centerY);
     for(var i = 1; i < path.length; i++) {
-      ctx.lineTo(path[i].x + centerX, path[i].y + centerY);
+      ctx.lineTo(path[i].x, path[i].y);
     }
     ctx.stroke();
   }
