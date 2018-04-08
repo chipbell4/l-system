f3355d00ae56d1ef5bd9a016223b7bd9c6d09343
Chip Bell <cbell@pbs.org>
2018-04-08 14:33:41 -0400

    Loading presets from config

--- ui.js
+++ ui.js
@@ -7,10 +7,32 @@ UI = {
     var template = UI.$('#rule-template > .rule').cloneNode(true);
     UI.$('#rules').appendChild(template);
     template.querySelector('.remove-rule').addEventListener('click', UI.removeRule.bind(UI));
+    return template;
   },
 
   removeRule: function(evt) {
     evt.target.parentElement.remove();
+  },
+
+  clearRules: function() {
+    UI.$('#rules').innerHTML = '';
+  },
+
+  writeRule: function(node, value, replacement) {
+    node.querySelector('.start').value = value;
+    node.querySelector('.end').value = replacement;
+  },
+
+  loadConfig: function(config) {
+    ['iterations', 'angle', 'angle-chaos', 'distance-chaos'].forEach(function(field) {
+      UI.$('#' + field).value = config[field];
+    });
+
+    UI.clearRules();
+    config.rules.forEach(function(rule) {
+      var node = UI.addRule();
+      UI.writeRule(node, rule.value, rule.replacement);
+    });
   },
 
   redraw: function() {
